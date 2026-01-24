a7f234e8429b8271f7951baadc469439f56c7b4d
donghung94 <nongaphao@gmail.com>
2026-01-24 14:55:24 +0900

    Delete questions1.js

--- questions1.js
+++
@@ -1,9 +0,0 @@
-window.questions = [
-  {
-    question: "問1. スプリンクラーは、なんの工事（こうじ）か",
-    options: ["海洋（かいよう）土木（どぼく）工事（こうじ）","通（つうしん）工事（こうじ）","土木（どぼく）工事（こうじ）","消防（しょうぼう）設備（せつび）工事（こうじ）"],
-    answer: 4,
-    explanation: "スプリンクラーは火災を消火するための消防設備です。\n→ Sprinkler thuộc về công trình thiết bị phòng cháy chữa cháy.",
-    translation: "Hệ thống Sprinkler thuộc loại công trình nào?"
-  }
-];
